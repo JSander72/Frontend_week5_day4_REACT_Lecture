6cb58672c48b443f4b184cae24940bda9c787e63
JSander72 <jamesearlsanders2@outlook.com>
2024-09-26 21:25:21 -0400

    initiate

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Greeting() {
+    //Verables and function go here (before return)
+
+    //Whatever you want to show on the screen goes here
+  return (
+    //JSX code goes here
+    // Two ways to write JSX code
+    <p>Welcome to the world if React!</p>
+        
+  )
+}
+
+export default Greeting
